61c7315604042e1afe92729cfd67b1841edc9253
Imshubhamlack <shubhamlakhamapure00@gmail.com>
2022-07-27 18:24:39 +0530

    Added java demo application files

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+System.out.println(checkInputIsAnEventNumber(122));
+	}
+	public static boolean checkInputIsAnEventNumber(int number) {
+		return number % 2==0;
+	}
+}
